72fd42a34195e93422c5e94c89aed6d67224b583
9671369-glitch <9671369@graniteschools.org>
2026-08-31 17:41:13 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://bennettg.xyz",
+  bare: "https://phantom.lol",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
